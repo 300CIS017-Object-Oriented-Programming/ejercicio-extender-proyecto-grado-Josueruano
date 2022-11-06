be26f7072c34ec9b3867e77c880e6ea97f268edd
Josue Ruano <josueruano1@javerianacali.edu.co>
2022-11-06 18:59:17 -0500

    New: se realizo los puntos 1,2,3, se agrego la informacion adicional y las restriccion de nota final

--- controller/ControladorPDF.py
+++ controller/ControladorPDF.py
@@ -24,12 +24,13 @@ class ControladorPdf:
         self.pdf.cell(200, 10, txt="ACTA DE EVALUACIÓN DE GRADO", ln=1, align='C')
         self.pdf.cell(200, 10, txt="Facultad de Ingeniería.", ln=2, align='C')
         self.pdf.set_font("times", size=12)
-        self.pdf.multi_cell(185, 6,
-                       txt="Trabajo de grado denominado: Análisis de la similitud en el código fuente de dos programas de computador usando técnicas de inteligencia artificial",
-                       align='L')
+        
         for acta in controlador.actas:
             if acta.autor == acta_seleccionada:
                 flag = True
+                self.pdf.multi_cell(185, 6,
+                       txt=f"Trabajo de grado denominado: {acta.nombre_trabajo}",
+                       align='L')
                 self.pdf.cell(200, 10, txt=f"Autor:                                {acta.autor}", ln=1, align='L')
                 self.pdf.cell(200, 10, txt=f"Fecha/Periodo:                  {acta.fecha_acta} Tesis II", ln=1, align='L')
                 self.pdf.cell(200, 10, txt=f"Director:                            {acta.director}", ln=1, align='L')
@@ -38,6 +39,7 @@ class ControladorPdf:
                 self.pdf.cell(200, 10, txt=f"Tipo De Trabajo:               {acta.tipo_trabajo}", ln=1, align='L')
                 self.pdf.cell(200, 10, txt=f"Jurado 1:                            {acta.jurado1}", ln=1, align='L')
                 self.pdf.cell(200, 10, txt=f"Jurado 2:                            {acta.jurado2}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"fecha de presentacion                 {acta.fecha_de_presentacion}", ln=1, align='L')
                 self.pdf.multi_cell(185, 6,
                                txt="En atención al desarrollo de este Trabajo de Grado y al documento y sustentación que presentó el(la) autor(a), los Jurados damos las siguientes calificaciones parciales y observaciones (los criterios a evaluar y sus ponderaciones se estipulan en el artículo 7.1 de las Directrices para Trabajo de Grado de Maestría):",
                                align='L')
@@ -64,7 +66,8 @@ class ControladorPdf:
                 self.pdf.cell(95, 10, txt="Números", ln=0, align='C')
                 self.pdf.cell(35, 10, txt="Letras", ln=1, align='C')
                 self.pdf.set_font("times", size=12)
-                self.pdf.multi_cell(185, 5, txt="Observaciones adicionales: ", align='L')
+                self.pdf.multi_cell(185, 5, txt="Observaciones adicionales: {acta.info_adicional} ", align='L')
+                self.pdf.multi_cell(185, 5, txt="Observaciones adicionales: {acta.restricciones_nota_final}", align='L')
                 self.pdf.multi_cell(185, 5,
                                txt="_____________________________________________________________________________________",
                                align='L')
@@ -88,7 +91,7 @@ class ControladorPdf:
                                align='L')
                 self.pdf.cell(200, 30, txt="________________________________   ________________________________", ln=2,
                          align='C')
-                self.pdf.cell(200, 5, txt="Firma Jurado 1                                      Firma Jurado 2 ", ln=2,
+                self.pdf.cell(200, 5, txt=f"{acta.jurado1}                                      Firma Jurado 2 ", ln=2,
                          align='C')
 
         if [acta.autor for acta in controlador.actas if acta.estado and flag]:
